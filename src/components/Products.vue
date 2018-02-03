3ba8eec44eb8c27301aa36f85744e6454c0229ba
Anangga Dipa <bugs1945@gmail.com>
2018-02-03 23:14:22 +0800

    before applying tiny mce

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,8 +1,7 @@
-/* eslint max-len: ["error", { "code": 800 }] */
 <template>
   <div class="row">
     <div class="col-12 col-sm-5 col-md-4 sidebar">
-      <h2>{{productListTitle}}</h2>
+      <h2>{{setup.productListTitle}}</h2>
 
       <ul>
         <li v-for="(product, index) in products"
@@ -15,57 +14,61 @@
     </div>
 
     <div class="col-12 col-sm-7 col-md-8">
-      <h2>{{productDetailTileSuffix}} {{products[currentProductIndex].name}}</h2>
+      <h2>{{setup.productDetailTileSuffix}} {{products[setup.currentProductIndex].name}}</h2>
 
-      <p>{{productTotalCost}} {{currency}} {{totalCost}}
+      <p>{{setup.productTotalCost}} {{setup.currency}} {{setup.totalCost}}
         <br />
-        {{productQttLabel}} {{products[currentProductIndex].qtt}}
+        {{setup.productQttLabel}} {{products[setup.currentProductIndex].qtt}}
         <br />
-        {{selingPriceSuggestion}} {{getSuggestionSellingPrice()}}
+        {{setup.selingPriceSuggestion}} {{getSuggestionSellingPrice()}}
         <br />
-        {{selingPriceSelected}} {{currency}}
-        <input v-model="products[currentProductIndex].sellingPrice" />
+        {{setup.selingPriceSelected}} {{setup.currency}}
+        <input v-model="products[setup.currentProductIndex].sellingPrice" />
 
         <span v-html="getSingleProfit()" />
         <span v-html="getTotalProfit()" />
       </p>
 
-      <h3>{{productIngredientsTitle}}</h3>
+      <h3>{{setup.productIngredientsTitle}}</h3>
 
       <ul>
         <li class="row"
-          v-for="i in products[currentProductIndex].ingredients"
+          v-for="i in products[setup.currentProductIndex].ingredients"
           v-bind:key="i.id"
-          @mouseover="i.active = true"
-          @mouseleave="i.active = false">
-          <div v-show="!i.edited">
+          @mouseover="setup.currentHover = setup.currentProductIndex +'-'+ i.id"
+          @mouseleave="resetHover()">
+          <div v-show="!isEdited(setup.currentProductIndex +'-'+ i.id)">
             {{i.qtt}}
             {{i.name}}
-            ({{currency}} {{i.price}})
+            ({{setup.currency}} {{i.price}})
           </div>
-          <div v-show="i.edited">
+          <div v-show="isEdited(setup.currentProductIndex +'-'+ i.id)">
             <input v-model="i.qtt" />
             <input v-model="i.name" />
             <input v-model="i.price" />
-            <span @click="i.edited = false" class="oi oi-check" title="icon done" aria-hidden="true"></span>
+            <span @click="resetEdited()" class="oi oi-check" title="icon done" aria-hidden="true"></span>
           </div>
-          <div v-show="i.active && !i.edited" class="editor">
-            <span @click="i.edited = true" class="oi oi-pencil" title="icon pencil" aria-hidden="true"></span>
+          <div v-show="isHover(setup.currentProductIndex +'-'+ i.id)" class="editor">
+            <span @click="setup.currentEdited = setup.currentProductIndex +'-'+ i.id" class="oi oi-pencil" title="icon pencil" aria-hidden="true"></span>
             <span class="oi oi-trash" title="icon trash" aria-hidden="true"></span>
           </div>
         </li>
       </ul>
 
-      <h3>{{productProcessingTitle}}</h3>
+      <h3>{{setup.productProcessingTitle}}</h3>
       <div
-        @mouseover="products[currentProductIndex].active = true"
-        @mouseleave="products[currentProductIndex].active = false">
-        <div v-show="products[currentProductIndex].active && !products[currentProductIndex].edited" class="editor">
-          <span @click="products[currentProductIndex].edited = true" class="oi oi-pencil" title="icon pencil" aria-hidden="true"></span>
+        @mouseover="setup.currentHover = setup.currentProductIndex"
+        @mouseleave="resetHover()">
+        <div v-show="isHover(setup.currentProductIndex)" class="editor">
+          <span @click="setup.currentEdited = setup.currentProductIndex" class="oi oi-pencil" title="icon pencil" aria-hidden="true"></span>
           <span class="oi oi-trash" title="icon trash" aria-hidden="true"></span>
         </div>
 
-        <div v-html="products[currentProductIndex].processing" />
+        <div v-show="!isEdited(setup.currentProductIndex)" v-html="products[setup.currentProductIndex].processing" />
+        <div v-show="isEdited(setup.currentProductIndex)">
+          <tiny-mce id="description" v-model="products[setup.currentProductIndex].processing"></tiny-mce>
+          <span @click="resetEdited()" class="oi oi-check" title="icon done" aria-hidden="true"></span>
+        </div>
       </div>
 
     </div>
@@ -76,73 +79,90 @@
 export default {
   name: 'ProductList',
   beforeMount() {
-    // on the fly var
-    this.totalCost = this.getTotalCost();
+    this.setup.totalCost = this.getTotalCost();
   },
   methods: {
     openDetail(i) {
-      this.currentProductIndex = i;
-      this.totalCost = this.getTotalCost();
+      this.setup.currentProductIndex = i;
+      this.setup.totalCost = this.getTotalCost();
     },
 
     getTotalCost() {
-      const totalCost = (this.products[this.currentProductIndex].ingredients)
+      const totalCost = (this.products[this.setup.currentProductIndex].ingredients)
         .reduce((total, item) => ({ price: (total.price) + (item.price) }));
 
       return totalCost.price;
     },
 
     getSuggestionSellingPrice() {
-      const unitCost = (this.totalCost / this.products[this.currentProductIndex].qtt) * 1;
-      return `${this.currency} ${unitCost + (unitCost * 0.20)}`;
+      const unitCost = (this.setup.totalCost / this.products[this.setup.currentProductIndex].qtt) * 1;
+      return `${this.setup.currency} ${unitCost + (unitCost * 0.20)}`;
     },
 
     getSingleProfit() {
-      const product = this.products[this.currentProductIndex];
+      const product = this.products[this.setup.currentProductIndex];
 
       if (product.sellingPrice <= 0) {
-        return `<br />${this.setPriceText}`;
+        return `<br />${this.setup.setPriceText}`;
       }
 
-      this.totalCost = typeof (this.totalCost) === 'undefined' ? this.getTotalCost() : this.totalCost;
-      return `<br />${this.singleProfitLabel} ${this.currency} ${product.sellingPrice - (this.totalCost / product.qtt)}`;
+      this.setup.totalCost = typeof (this.setup.totalCost) === 'undefined' ? this.getTotalCost() : this.setup.totalCost;
+      return `<br />${this.setup.singleProfitLabel} ${this.setup.currency} ${product.sellingPrice - (this.setup.totalCost / product.qtt)}`;
     },
 
     getTotalProfit() {
-      const product = this.products[this.currentProductIndex];
+      const product = this.products[this.setup.currentProductIndex];
 
       if (product.sellingPrice > 0) {
-        this.totalCost = typeof (this.totalCost) === 'undefined' ? this.getTotalCost() : this.totalCost;
-        return `<br />${this.totalProfitLabel} ${this.currency} ${(product.sellingPrice * product.qtt) - this.totalCost}`;
+        this.setup.totalCost = typeof (this.setup.totalCost) === 'undefined' ? this.getTotalCost() : this.setup.totalCost;
+        return `<br />${this.setup.totalProfitLabel} ${this.setup.currency} ${(product.sellingPrice * product.qtt) - this.setup.totalCost}`;
       }
 
       return '';
     },
 
     getActiveClass(i) {
-      return i === this.currentProductIndex ? 'active' : '';
+      return i === this.setup.currentProductIndex ? 'active' : '';
+    },
+
+    isHover(id) {
+      return id === this.setup.currentHover && id !== this.setup.currentEdited;
+    },
+
+    isEdited(id) {
+      return id === this.setup.currentEdited;
+    },
+
+    resetHover() {
+      this.setup.currentHover = -1;
+    },
+
+    resetEdited() {
+      this.setup.currentEdited = -1;
     },
   },
   data() {
     return {
-      currency: 'Rp',
-      totalCost: 0,
-      currentProductIndex: 0,
-      productListTitle: 'Daftar Produk',
-      productDetailTileSuffix: 'Detil',
-      productTotalCost: 'Total pengeluaran:',
-      productQttLabel: 'Jumlah produk jadi:',
-      selingPriceSuggestion: 'Harga jual persatuan yang disarankan',
-      selingPriceSelected: 'Harga jual persatuan yang digunakan',
-      singleProfitLabel: 'Keuntungan persatuan:',
-      totalProfitLabel: 'Keuntungan perproduksi:',
-      setPriceText: 'Masukkan harga jual satuan untuk melihat keuntungan.',
-      productIngredientsTitle: 'Bahan-bahan dan Kebutuhan',
-      productProcessingTitle: 'Cara memasak',
+      setup: {
+        currency: 'Rp',
+        productListTitle: 'Daftar Produk',
+        productDetailTileSuffix: 'Detil',
+        productTotalCost: 'Total pengeluaran:',
+        productQttLabel: 'Jumlah produk jadi:',
+        selingPriceSuggestion: 'Harga jual persatuan yang disarankan',
+        selingPriceSelected: 'Harga jual persatuan yang digunakan',
+        singleProfitLabel: 'Keuntungan persatuan:',
+        totalProfitLabel: 'Keuntungan perproduksi:',
+        setPriceText: 'Masukkan harga jual satuan untuk melihat keuntungan.',
+        productIngredientsTitle: 'Bahan-bahan dan Kebutuhan',
+        productProcessingTitle: 'Cara memasak',
+        totalCost: 0,
+        currentProductIndex: 0,
+        currentHover: -1,
+        currentEdited: -1,
+      },
       products: [
         {
-          active: false,
-          edited: false,
           name: 'pisang goreng',
           slug: 'pisang-goreng',
           qtt: 20,
@@ -151,8 +171,6 @@ export default {
           ingredients: [
             {
               id: 0,
-              active: false,
-              edited: false,
               name: 'pisang',
               qtt: '10 buah',
               price: 5000,
@@ -160,8 +178,6 @@ export default {
 
             {
               id: 1,
-              active: false,
-              edited: false,
               name: 'tepung',
               qtt: '100gr',
               price: 3000,
@@ -169,8 +185,6 @@ export default {
 
             {
               id: 2,
-              active: false,
-              edited: false,
               name: 'minyak goreng',
               qtt: '1/2 liter',
               price: 15000,
@@ -178,8 +192,6 @@ export default {
 
             {
               id: 3,
-              active: false,
-              edited: false,
               name: 'gas',
               qtt: 'secukupnya',
               price: 6000,
@@ -187,8 +199,6 @@ export default {
           ],
         },
         {
-          active: false,
-          edited: false,
           name: 'tape goreng',
           slug: 'tape',
           qtt: 20,
@@ -197,8 +207,6 @@ export default {
           ingredients: [
             {
               id: 0,
-              active: false,
-              edited: false,
               name: 'tape',
               qtt: '2 bungkus',
               price: 7000,
@@ -206,8 +214,6 @@ export default {
 
             {
               id: 1,
-              active: false,
-              edited: false,
               name: 'tepung',
               qtt: '100gr',
               price: 3000,
@@ -215,8 +221,6 @@ export default {
 
             {
               id: 2,
-              active: false,
-              edited: false,
               name: 'minyak goreng',
               qtt: '1/2 liter',
               price: 15000,
@@ -224,8 +228,6 @@ export default {
 
             {
               id: 3,
-              active: false,
-              edited: false,
               name: 'gas',
               qtt: 'secukupnya',
               price: 6000,
